f56c8bfaa50bbff95e981b040b662f0242b4ff41
KAkhoa12 <90773513+KAkhoa12@users.noreply.github.com>
2023-02-03 12:51:25 +0700

    add admin

--- KTX_HUFI_2023/Areas/Admin/Controllers/HomeController.cs
+++ KTX_HUFI_2023/Areas/Admin/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ namespace KTX_HUFI_2023.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        [Area("Admin")]
         public IActionResult Index()
         {
             return View();
