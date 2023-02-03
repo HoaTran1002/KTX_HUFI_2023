b23a9ad56510bc1e7334f3f6d394ec580c88cea8
KAkhoa12 <90773513+KAkhoa12@users.noreply.github.com>
2023-02-03 12:25:55 +0700

    update areas admin

--- KTX_HUFI_2023/Program.cs
+++ KTX_HUFI_2023/Program.cs
@@ -23,5 +23,11 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "Admin",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 app.Run();
