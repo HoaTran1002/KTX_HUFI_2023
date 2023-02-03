f35f4170b00a06b101ab2ddac50071c12c23a077
moscow <vanhoa0908751520@gmail.com>
2023-02-03 08:11:49 +0700

    Add project files.

---
+++ KTX_HUFI_2023/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KTX_HUFI_2023.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
