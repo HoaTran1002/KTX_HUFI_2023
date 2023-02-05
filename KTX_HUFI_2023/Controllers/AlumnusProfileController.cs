0d8f5662d769a299f633f438f471c9e278d45539
KAkhoa12 <90773513+KAkhoa12@users.noreply.github.com>
2023-02-05 18:47:08 +0700

    add page Home Alumnus and Profile Alumnus

---
+++ KTX_HUFI_2023/Controllers/AlumnusProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KTX_HUFI_2023.Controllers
+{
+    public class AlumnusProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
